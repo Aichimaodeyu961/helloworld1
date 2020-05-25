f19ff2793c3ca73a75066b741ce5d0ec3d98d641
梁化海 <2814688636@qq.com>
2020-05-25 09:30:51 +0800

    grfdgdf

--- src/Hellowo2.java
+++ src/Hellowo2.java
@@ -2,5 +2,6 @@ public class Hellowo2 {
     public static void main(String[] args) {
         System.out.println("this is two");
         System.out.println("fsadfas");
+        System.out.println("esafd");
     }
 }
